54ffb51487012ccd5ce3cbd6b02773c9710871d5
Eduardo <eduardo.silva@icloud.com>
2025-03-26 20:34:14 -0300

    Refatoracao

--- src/app/investment/asset/irpf/irpf.component.ts
+++ src/app/investment/asset/irpf/irpf.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AssetServiceImpl } from '../service/impl/asset-impl.service';
 import { IrpfModel } from './model/irpf-model';
 import { CommonModule } from '@angular/common';
@@ -10,10 +10,11 @@ import { CommonModule } from '@angular/common';
   templateUrl: './irpf.component.html',
   styleUrl: './irpf.component.css'
 })
-export class IrpfComponent implements OnInit{
+export class IrpfComponent implements OnInit {
   @Input()
   parentId!: number;
   model!: IrpfModel;
+  ticker!: string;
   constructor(private service: AssetServiceImpl) {
 
   }
@@ -21,6 +22,9 @@ export class IrpfComponent implements OnInit{
     this.service.irpf(this.parentId, 2024).subscribe((response) => {
       console.log(response);
       this.model = response;
+    });
+    this.service.findById(this.parentId).subscribe((response) => {
+      this.ticker = response.ticker;
     });
   }
 
