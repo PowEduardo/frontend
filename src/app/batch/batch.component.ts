2f128b6452b36bcb39f6f92e338193c012ee5631
Eduardo <eduardo.silva@icloud.com>
2025-09-08 14:35:22 -0300

    Config url host on batch component

--- src/app/batch/batch.component.ts
+++ src/app/batch/batch.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Execution } from './model/execution';
 import { Step } from './model/step';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-batch',
@@ -24,14 +25,14 @@ export class BatchComponent {
   }
 
   fetchJobs(): void {
-    this.http.get<string[]>('http://localhost:8080/jobs').subscribe((data) => {
+    this.http.get<string[]>(environment.apiBaseUrl+ '/jobs').subscribe((data) => {
       this.jobs = data;
     });
   }
 
   viewExecutions(jobName: string): void {
     this.selectedJob = jobName;
-    this.http.get<Execution[]>(`http://localhost:8080/jobs/${jobName}/executions`).subscribe((data) => {
+    this.http.get<Execution[]>(environment.apiBaseUrl+ `/jobs/${jobName}/executions`).subscribe((data) => {
       this.executions = data.map((execution) => {
         var exec = new Execution(execution.job_execution_id, execution.status, execution.start_time, execution.end_time, execution.stepDetails);
         exec.totalTime = (execution.end_time.getTime() - execution.start_time.getTime()) / 1000;
@@ -48,7 +49,7 @@ export class BatchComponent {
       const execution = this.executions.find((e) => e.job_execution_id === executionId);
       if (execution && execution.stepDetails.length === 0) {
         // Fetch step details if not already loaded
-        this.http.get<Step[]>(`http://localhost:8080/executions/${executionId}/steps`).subscribe((data) => {
+        this.http.get<Step[]>(environment.apiBaseUrl+ `/executions/${executionId}/steps`).subscribe((data) => {
           execution.stepDetails = data;
         });
       }
